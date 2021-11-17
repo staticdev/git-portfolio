460e8bef7bb1d3308b3fbae3591ca06e6241cdbd
staticdev <staticdev-support@protonmail.com>
2021-11-17 18:56:17 +0100

    Fix git commit error output

--- tests/use_cases/test_git.py
+++ tests/use_cases/test_git.py
@@ -23,28 +23,27 @@ def mock_popen(mocker: MockerFixture) -> MockerFixture:
 def test_execute_success(mock_popen: MockerFixture) -> None:
     """It returns success messages."""
     response = git.GitUseCase().execute(
-        ["staticdev/omg", "staticdev/omg2"], "checkout", ("xx",)
+        ["user/repo", "user/repo2"], "checkout", ("xx",)
     )
 
     assert bool(response) is True
-    assert response.value == "omg: some output\nomg2: some output\n"
+    assert response.value == "repo: some output\nrepo2: some output\n"
 
 
 def test_execute_success_no_output(mock_popen: MockerFixture) -> None:
     """It returns success message."""
     mock_popen.return_value.communicate.return_value = (b"", b"")
-    response = git.GitUseCase().execute(
-        ["staticdev/omg", "staticdev/omg2"], "add", (".",)
-    )
+    # case where command success has no output with `gitp add .`
+    response = git.GitUseCase().execute(["user/repo", "user/repo2"], "add", (".",))
 
     assert bool(response) is True
-    assert response.value == "omg: add successful.\nomg2: add successful.\n"
+    assert response.value == "repo: add successful.\nrepo2: add successful.\n"
 
 
 def test_execute_git_not_installed(mock_command_checker: MockerFixture) -> None:
     """It returns failure with git not installed message."""
     mock_command_checker.return_value = "error"
-    response = git.GitUseCase().execute(["staticdev/notcloned"], "checkout", ("xx",))
+    response = git.GitUseCase().execute(["user/dontcare"], "checkout", ("xx",))
 
     assert bool(response) is False
     assert "error" == response.value["message"]
@@ -58,7 +57,7 @@ def test_execute_no_folder(
     mock_exception = FileNotFoundError(2, "No such file or directory")
     mock_exception.filename = "/path/x"
     mock_popen.side_effect = mock_exception
-    response = git.GitUseCase().execute(["staticdev/x"], "checkout", ("xx",))
+    response = git.GitUseCase().execute(["user/x"], "checkout", ("xx",))
 
     assert bool(response) is True
     assert response.value == "x: No such file or directory: /path/x\n"
@@ -69,12 +68,35 @@ def test_execute_error_during_execution(mock_popen: MockerFixture) -> None:
     mock_popen.return_value.returncode = 1
     mock_popen().communicate.return_value = (
         b"",
-        b"error: pathspec 'xyz' did not match any file(s) known to git",
+        b"fatal: A branch named 'existingbranch' already exists.",
     )
-    response = git.GitUseCase().execute(["staticdev/notcloned"], "checkout", ("xyz",))
+    # case where create branch already exists with `gitp checkout -b existingbranch`
+    response = git.GitUseCase().execute(
+        ["user/cloned"], "checkout", ("-b", "existingbranch")
+    )
 
     assert bool(response) is True
     assert (
         response.value
-        == "notcloned: error: pathspec 'xyz' did not match any file(s) known to git"
+        == "cloned: fatal: A branch named 'existingbranch' already exists."
+    )
+
+
+def test_execute_error_no_stderr(mock_popen: MockerFixture) -> None:
+    """It returns success message with the stdout on output."""
+    mock_popen.return_value.returncode = 1
+    mock_popen().communicate.return_value = (
+        (
+            b"On branch main\nYour branch is up to date with 'origin/main'.\n\n"
+            b"nothing to commit, working tree clean"
+        ),
+        b"",
+    )
+    # case where is nothing to commit with `gitp commit`
+    response = git.GitUseCase().execute(["user/cloned"], "commit", ("",))
+
+    assert bool(response) is True
+    assert response.value == (
+        "cloned: On branch main\nYour branch is up to date with 'origin/main'.\n\n"
+        "nothing to commit, working tree clean\n"
     )
