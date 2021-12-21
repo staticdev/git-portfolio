488b5183e17573b05bd11957e22a9b1c868c50e2
staticdev <staticdev-support@protonmail.com>
2021-12-21 18:59:05 +0100

    Workaround for linkcheck bug

--- docs/conf.py
+++ docs/conf.py
@@ -3,8 +3,12 @@ from datetime import datetime
 
 
 project = "Git Portfolio"
-author = "Thiago Carvalho D'Ávila"
+author = "staticdev"
 copyright = f"{datetime.now().year}, {author}"
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_click"]
 autodoc_typehints = "description"
 html_theme = "furo"
+linkcheck_ignore = [  # temporary workaround
+    "codeofconduct.html",
+    "contributing.html",
+]
